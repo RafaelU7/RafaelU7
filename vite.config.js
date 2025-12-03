39ca9b527627d5e1c4b7067de3c86972c1d2f539
Prime Landing Studio <dev@primelandingstudio.com>
2025-12-02 21:03:27 -0300

    Usar minificação padrão do Vite (esbuild) em vez de terser

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      }
-    },
     rollupOptions: {
       output: {
         manualChunks: {
